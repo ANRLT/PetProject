e092a8de40078d9a269244e1dce8bec9b273d473
Alexander Zhurauskij <sashkoo9005@mail.ru>
2025-02-22 16:46:08 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPetProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
